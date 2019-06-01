ff486d52e21150c7f67768bbe26d527741d032c1
valeriazamora <valezamora@gmail.com>
2019-06-01 00:03:02 -0600

    Definicion de clases

---
+++ src/router.py
@@ -0,0 +1,26 @@
+import socket
+import ruta
+
+
+class Router:
+    tablaDeRutas = []
+    listaDeAdyacencia = []
+
+    def __init__(self, id):
+        self.id = id
+
+    def compartir(self):
+        return None
+
+    def actualizarTabla(self):
+        return None
+
+    def imprimirTabla(self):
+        return None
+
+    def leerVecinos(self):
+        return None
+
+    def splitHorizon(self):
+        return None
+
---
+++ src/ruta.py
@@ -0,0 +1,6 @@
+class Ruta:
+    def __init__(self, red, mascara, siguiente, distancia):
+        self.red = red
+        self.mascara = mascara
+        self.siguiente = siguiente
+        self.distancia = distancia
