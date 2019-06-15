6b5ab7f3a2f8504271f2a3ad458dd7f41d3272b5
Esteban201483 <esteban201483@gmail.com>
2019-06-15 11:57:05 -0600

    Limpieza de código y codificación/decodificación de mensajes obligatoria en python 3

--- src/prueba.py
+++ src/prueba.py
@@ -7,6 +7,7 @@ import ruta
 HOST = '224.3.29.72'
 ports = [2001 + i * 0 for i in range(0,11)]
 sockets = [[] for i in range(0,6)] #Indice 0 es para el socket que envia y el indice 1 es para el socket que recibe datos
+principal_port = 2001
 
 # Instancias de tabla de rutas
 tablaRuta1 = []
@@ -16,8 +17,6 @@ tablaRuta4 = []
 tablaRuta5 = []
 kill3 = False
 
-
-
 def compartir():
     nombre = threading.current_thread().getName()
     count = 0
@@ -25,25 +24,25 @@ def compartir():
         while (count < 3):
             print('COMPARTE3')
             # Send tablaRutas3
-            sent = sockets[3][0].sendto("Prueba", (HOST,2001))
+            sent = sockets[3][0].sendto("Test 3".encode(), (HOST,principal_port))
             count += 1
 
     else:
         while (count < 10):
             if (threading.current_thread().getName() == 'comparte1'):
-                sent = sockets[1][0].sendto("Prueba", (HOST, 2001))
+                sent = sockets[1][0].sendto("Prueba 1".encode(), (HOST, principal_port))
                 print('COMPARTE1')
                 # Send tablaRutas1
             elif (threading.current_thread().getName() == 'comparte2'):
-                sent = sockets[2][0].sendto("Prueba", (HOST, 2002))
+                sent = sockets[2][0].sendto("Mensaje 2".encode(), (HOST, principal_port))
                 print('COMPARTE2')
                 # Send tablaRutas2
             elif (threading.current_thread().getName() == 'comparte4'):
-                sent = sockets[4][0].sendto("Prueba", (HOST, 2004))
+                sent = sockets[4][0].sendto("Hilera 4".encode(), (HOST, principal_port))
                 print('COMPARTE4')
                 # Send tablaRutas4
             elif (threading.current_thread().getName() == 'comparte5'):
-                sent = sockets[5][0].sendto("Prueba", (HOST, 2005))
+                sent = sockets[5][0].sendto("Texto 5".encode(), (HOST, principal_port))
                 print('COMPARTE5')
                 # Send tablaRutas5
             count += 1
@@ -64,25 +63,27 @@ def recibir():
                 if (threading.current_thread().getName() == 'recibe1'):
                     print("recibe1")
                     dato, address = sockets[1][1].recvfrom(1024)
-                    print("Recibe 1 exitoso")
+                    dato = dato.decode()
                     print(dato)
                     # Send tablaRutas1
                 elif (threading.current_thread().getName() == 'recibe2'):
                     print("recibe2")
                     dato , address = sockets[2][1].recvfrom(1024)
-                    print("Recibe 2 exitoso")
+                    dato = dato.decode()
                     print(dato)
                     # Send tablaRutas2
-                elif (threading.current_thread().getName() == 'recibe43'):
+                elif (threading.current_thread().getName() == 'recibe4'):
 
                     #sockets[4][1].bind(server_address)
                     dato, address = sockets[4][1].recvfrom(1024)
+                    dato = dato.decode()
                     print(dato)
                     # Send tablaRutas4
-                elif (threading.current_thread().getName() == 'recibe53'):
+                elif (threading.current_thread().getName() == 'recibe5'):
 
                     #sockets[5][1].bind(server_address)
                     dato, address = sockets[5][1].recvfrom(1024)
+                    dato = dato.decode()
                     print(dato)
                     # Send tablaRuta
             except:
@@ -99,7 +100,7 @@ for num_hilo in range(1,6):
     ttl = struct.pack('b', 5)
     sock_envio.setsockopt(IPPROTO_IP, IP_MULTICAST_TTL, ttl)
 
-    grupo_recepcion = (HOST, 2001)
+    grupo_recepcion = (HOST, principal_port)
     sock_recibido = socket(AF_INET, SOCK_DGRAM)
     #settimeout(0.2)
     ttl = struct.pack('b', 1)
@@ -110,30 +111,12 @@ for num_hilo in range(1,6):
     mreq = struct.pack('4sL', group, INADDR_ANY)
     sock_recibido.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, mreq)
 
-
-    print(num_hilo)
     sockets[num_hilo].append(sock_envio)
     sockets[num_hilo].append(sock_recibido)
-
-
-
-    server_address = ('', ports[11-num_hilo])
-    server_address2 = ('', ports[11 - num_hilo])
-    server_address3 = ('', ports[11 - num_hilo])
-    server_address4 = ('', ports[11 - num_hilo])
-    server_address5 = ('', ports[11 - num_hilo])
-
-
-
-    if(num_hilo == 1):
-        sockets[num_hilo][1].setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        sockets[1][1].bind(server_address)
-    if(num_hilo == 2):
-        sockets[2][1].setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        sockets[2][1].bind(server_address2)
-
-
-    #
+    sockets[num_hilo][1].setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    server_address = ('', ports[11 - num_hilo])
+    sockets[num_hilo][1].setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    sockets[num_hilo][1].bind(server_address)
 
     hilo1 = threading.Thread(name='comparte%s' % num_hilo,
                             target=compartir)
@@ -141,6 +124,5 @@ for num_hilo in range(1,6):
                             target=recibir)
     hilo1.start()
     hilo2.start()
-
 
 
