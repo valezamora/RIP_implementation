791f87dd774607c2e076aa35db78bf06cec1b5c8
valeriazamora <valezamora@gmail.com>
2019-06-15 08:51:54 -0600

    Logica del agregar

--- src/tabla_rutas.py
+++ src/tabla_rutas.py
@@ -1,4 +1,4 @@
-import ruta
+from ruta import Ruta
 
 
 class TablaRutas:
@@ -9,12 +9,24 @@ class TablaRutas:
         self.id = i
 
     # Agrega una ruta a la tabla de rutas
-    def agregar_ruta(self, ruta):
-        self.rutas.append(ruta)
+    def agregar_ruta(self, ruta2):
+        encontrado = False
+        for ruta in self.rutas:
+            if ruta.red == ruta2.red:
+                ruta.siguiente = ruta2.siguiente
+                ruta.distancia = ruta.distancia
+                encontrado = True
+
+        if not encontrado:
+            self.rutas.append(ruta2)
 
     # Verifica si una ruta existe dentro de la tabla de rutas
     def existe(self, ruta2):
-        print('running existe ruta?')
+        found = False
+        for ruta in self.rutas:
+            if (ruta.red == ruta2.red):
+                found = True
+        return found
 
     # Convierte la tabla de rutas a un string
     # Retorna: String que contiene la informacion de la tabla de rutas cada ruta separada por un ';'
@@ -23,28 +35,36 @@ class TablaRutas:
         for r in range(len(self.rutas)):
             string += self.rutas[r].ruta_to_string()
             string += ';'
-        print(string)
         return string
 
     # Convierte un string a una tabla de rutas
     def string_to_tabla(self, s):
-        print('string to tabla')
-        splitted = s
+        splitted = s.split(';')
+        new_routes = []
+        for r in splitted:
+            data = r.split(',')
+            new_routes.append(Ruta(data[0], data[1], data[3], data[3]))
+        return new_routes
 
     # Recibe un string de tabla de rutas y actualiza la tabla propia con la informacion recibida
     def actualizar_tabla(self, tabla_string):
-        print('actualizando tabla')
-        self.string_to_tabla(tabla_string)
-        for r in self.rutas:
-            print('recibiendo ')
+        received_routes = self.string_to_tabla(tabla_string)
+        for r in received_routes:
+            self.agregar_ruta(r)
+
+        self.imprimirTabla()
 
     # Imprime la tabla de rutas
     def imprimirTabla(self):
-        print('imprimirTabla')
+        print('Tabla ' + str(self.id))
+        print('Direccion\tMascara\t\t\tSiguiente\t\t\tDistancia')
+        for ruta in self.rutas:
+            print(ruta.red + '\t' + ruta.mascara + '\t' + ruta.siguiente + '\t\t' + ruta.distancia)
 
 
 # Start execution
-rr = ruta.Ruta('1', '2', '3', '4')
-tabla = TablaRutas()
+rr = Ruta('10.0.1.0', '255.255.255.0', '192.111.111.11', '4')
+tabla = TablaRutas(1)
 tabla.agregar_ruta(rr)
 tabla.get_rutas()
+tabla.actualizar_tabla('10.0.1.0,255.255.255.0,1,1;2,255.255.255.0,2,2')
